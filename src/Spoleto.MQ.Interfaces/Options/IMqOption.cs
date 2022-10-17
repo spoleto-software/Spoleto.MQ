3724942c48cffc1cc0d923cab05131a2e064cd97
Oleg Nadymov <OlegNadymov@gmail.com>
2022-10-17 17:51:09 +0300

    Comments

--- src/Spoleto.MQ.Interfaces/Options/IMqOption.cs
+++ src/Spoleto.MQ.Interfaces/Options/IMqOption.cs
@@ -5,10 +5,19 @@
     /// </summary>
     public interface IMqOption
     {
+        /// <summary>
+        /// Gets or sets the host url.
+        /// </summary>
         string Host { get; set; }
 
+        /// <summary>
+        /// Gets or sets Rabbit MQ username.
+        /// </summary>
         string Username { get; set; }
 
+        /// <summary>
+        /// Gets or sets password for Rabbit MQ user.
+        /// </summary>
         string Password { get; set; }
     }
 }
